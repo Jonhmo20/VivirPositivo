67591463d50f5fd7b474561a975f57064e1e8ecf
Jonh Morales <jonh.edu20@gmail.com>
2025-01-11 15:27:19 -0500

    Agrega mensaje de bienvenida en el componente Blogs

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -21,7 +21,20 @@ const Blogs: React.FC<BlogProps> = ({ blogs, onDelete, onEdit }) => {
 
     return (
         <div className="w-full min-h-screen p-10 bg-cerceta-light">
-           <h1 className="text-3xl font-bold mb-6"> </h1>
+          <div className="px-4 py-12 bg-gradient-to-b from-teal-10 to-teal-50/30">
+          <div className="max-w-4xl mx-auto">
+            <h1 className="text-4xl md:text-5xl font-bold text-center text-gray-800 mb-8 tracking-tight"> 
+              Vivir Positivo Blog
+            </h1>
+
+            <p className="max-w-3xl mx-auto text-center text-lg md:text-xl leading-relaxed text-gray-700/90">
+              Bienvenidos a nuestro espacio de aprendizaje y apoyo mutuo. Aquí compartimos 
+              conocimientos, experiencias y recursos sobre salud sexual, creando un entorno 
+              donde la educación y la comprensión rompen estigmas. Juntos, construimos una 
+              comunidad más informada, empática y saludable. Tu bienestar es nuestra prioridad.
+            </p>
+          </div>
+        </div>
            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
             {blogs.map((blog) => (
               <Blogcard
